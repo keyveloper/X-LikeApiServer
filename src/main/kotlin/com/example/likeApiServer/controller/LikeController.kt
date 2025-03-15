f6ff5bf80e8a9a5e660a77c8c6bacc1fdd352d8d
zen <didehdrb21@naver.com>
2025-03-15 15:01:01 +0900

    test yml added

--- src/main/kotlin/com/example/likeApiServer/controller/LikeController.kt
+++ src/main/kotlin/com/example/likeApiServer/controller/LikeController.kt
@@ -10,8 +10,6 @@ import com.example.likeApiServer.enum.MSAServerErrorCode
 import com.example.likeApiServer.service.LikeService
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
--- src/test/kotlin/com/example/likeApiServer/LiveApiServerApplicationTests.kt
+++ src/test/kotlin/com/example/likeApiServer/LiveApiServerApplicationTests.kt
@@ -2,8 +2,8 @@ package com.example.likeApiServer
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
-@SpringBootTest
 class LiveApiServerApplicationTests {
 
     @Test
